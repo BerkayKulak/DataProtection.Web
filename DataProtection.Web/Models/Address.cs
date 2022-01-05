b0c9b7aa463ec6a64287eb72372cc45660fd3ece
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-05 08:04:50 -0700

    Data Protection

---
+++ DataProtection.Web/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DataProtection.Web.Models
+{
+    public partial class Address
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Province { get; set; }
+        public string PostCode { get; set; }
+        public int? HouseId { get; set; }
+    }
+}
---
+++ DataProtection.Web/Models/ExampleDb2Context.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DataProtection.Web.Models
+{
+    public partial class ExampleDb2Context : DbContext
+    {
+        public ExampleDb2Context()
+        {
+        }
+
+        public ExampleDb2Context(DbContextOptions<ExampleDb2Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Address> Addresses { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.HasIndex(e => e.HouseId, "IX_Addresses_HouseId");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
