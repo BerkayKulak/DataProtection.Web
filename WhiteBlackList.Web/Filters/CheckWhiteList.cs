5ba32a0eb2cfe7f1da488441c0a61e5378e6f5aa
kulakberkay15@gmail.com <kulakberkay15@gmail.com>
2022-01-06 11:37:32 -0700

    WhiteList/BlackList IP control-2

---
+++ WhiteBlackList.Web/Filters/CheckWhiteList.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using WhiteBlackList.Web.Middleware;
+
+namespace WhiteBlackList.Web.Filters
+{
+    // filterlar sayesinde bir controllerdaki herhangi bir metoda istek gelmeden önce veya sonra, filterlar sayesinde gelen istekleri yakalayabiliyoruz.
+    // metoda veya controllera girmeden gelen isteği yakalıcam
+    public class CheckWhiteList:ActionFilterAttribute
+    {
+        private readonly IPList _ipList;
+
+        public CheckWhiteList(IOptions<IPList> ipList)
+        {
+            _ipList = ipList.Value;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var reqIpAdress = context.HttpContext.Connection.RemoteIpAddress;
+            var isWhiteList = _ipList.WhiteList.Where(x => IPAddress.Parse(x).Equals(reqIpAdress)).Any();
+
+            if (!isWhiteList)
+            {
+                context.Result = new StatusCodeResult((int) HttpStatusCode.Forbidden);
+                return;
+            }
+
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
